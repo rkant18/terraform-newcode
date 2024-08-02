a31c5d39e8ea4beeccfd54fc18d77200d911c03d
rkant18 <k46690968@gmail.com>
2024-08-02 12:50:19 +0100

    added update in vnet variable

--- modules/vnet/variable.tf
+++ modules/vnet/variable.tf
@@ -1,11 +1,3 @@
 variable "vnet" {
-  type = map(object({
-    name          = string
-    location           = string
-    resource_group_name = string
-    subnets = map(object({
-      name           = string
-      address_prefix = string
-    }))
-  }))
+  type = map(any)
 }
